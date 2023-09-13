55169287e013635b6a4debc1a51a64d287aec948
geeksusma <jesus.maria.villar.vazquez@gmail.com>
2023-09-13 15:42:19 +0200

    project from the spring initializr

---
+++ src/main/java/es/geeksusma/workshops/PersistenceTestsApplication.java
@@ -0,0 +1,13 @@
+package es.geeksusma.workshops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenceTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistenceTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/geeksusma/workshops/PersistenceTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package es.geeksusma.workshops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenceTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
